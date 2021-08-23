cba43e54a71cba39ed9dcffd47131538145e41c9
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 14:07:06 +0300

    dkdk

--- src/gps.js
+++ src/gps.js
@@ -1,6 +1,17 @@
-const Gpio = require("onoff").Gpio;
-const led = new Gpio(10, "out");
+var SerialPort = require("serialport");
+var port = new SerialPort.SerialPort("/dev/ttyUSB1", {
+  // change path
+  baudrate: 4800,
+  parser: SerialPort.parsers.readline("\r\n"),
+});
 
-led.watch((result) => {
-  console.log({ rrrrrrrr: result });
+var GPS = require("gps");
+var gps = new GPS();
+
+gps.on("data", function (data) {
+  console.log(data, gps.state);
+});
+
+port.on("data", function (data) {
+  gps.updatePartial(data);
 });
