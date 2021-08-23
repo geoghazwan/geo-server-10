2aa3728f4320b4f5d1f2cd2d8b401099742954d0
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 14:24:28 +0300

    dkdk

--- src/gps.js
+++ src/gps.js
@@ -2,13 +2,7 @@ const SerialPort = require("serialport");
 const Readline = SerialPort.parsers.Readline;
 const port = new SerialPort("/dev/ttyUSB1", { baudRate: 4800 });
 const parser = new Readline("\r\n");
-var GPS = require("gps");
-var gps = new GPS();
 port.pipe(parser);
-
-gps.on("data", function (data) {
-  console.log(data);
-});
 
 port.on("data", function (data) {
   console.log({ data });
