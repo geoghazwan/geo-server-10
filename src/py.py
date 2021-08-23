7116897a5bae0049095288e3ae36a5d52733c103
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 15:18:02 +0300

    Gkgkg

--- src/py.py
+++ src/py.py
@@ -1,8 +1,15 @@
 import gps
 import time
 
-session = gps.gps("127.0.0.1", "2947")
+
+def main():
+    session = gps.gps("127.0.0.1", "2947")
+
+
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 raw_data = session.next()
 if raw_data["class"] == "TPV":
     print(raw_data)
+
+if __name__ == "__main__":
+    main()
