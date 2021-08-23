42fc64fdba3ab01174545b5dc3628c75320ea6f6
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 15:55:41 +0300

    Gkgkg

--- src/utils/runScript.ts
+++ src/utils/runScript.ts
@@ -1,6 +1,7 @@
 import { pythonScripts } from "../scripts";
 import { promisify } from "util";
 const exec = promisify(require("child_process").exec);
+import { PythonShell } from "python-shell";
 
 export const runScript = async (script: string): Promise<void> => {
   const output = await exec(script);
@@ -9,14 +10,7 @@ export const runScript = async (script: string): Promise<void> => {
 };
 
 export const runPy = new Promise(function (success, nosuccess) {
-  const { spawn } = require("child_process");
-  const pyprog = spawn("sudo python", ["./../pypy.py"]);
-
-  pyprog.stdout.on("data", function (data: any) {
-    success(data);
-  });
-
-  pyprog.stderr.on("data", (data: any) => {
-    nosuccess(data);
+  PythonShell.runString("print 1", {}, (error: any, output: any) => {
+    console.log({ error, output });
   });
 });
