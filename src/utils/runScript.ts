730dc0b3ddba463da0f63c826503ccf359efc9ec
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 17:03:48 +0300

    Gkgkg

--- src/utils/runScript.ts
+++ src/utils/runScript.ts
@@ -1,35 +1,9 @@
 import { promisify } from "util";
 const exec = promisify(require("child_process").exec);
 import { PythonShell } from "python-shell";
-import path from "path"
+import path from "path";
 export const runScript = async (script: string): Promise<void> => {
-  const output = await exec(`python ${path.resolve(__dirname, "py2.py")});
+  const output = await exec(`python ${path.resolve(__dirname, "py2.py")}`);
   console.log({ ooooooooo: output });
   return output;
-};
-
-const script = `E=quit
-D=print
-import gps as A,time
-B=A.gps('127.0.0.1','2947')
-B.stream(A.WATCH_ENABLE|A.WATCH_NEWSTYLE)
-while True:
-	try:
-		time.sleep(0.5);C=B.next()
-		if C['class']=='TPV':
-			if hasattr(C,'lat'):D(str(C.lat));E()
-	except KeyError:pass
-	except KeyboardInterrupt:E()
-	except StopIteration:B=None;D('No incoming data from the GPS module')`;
-
-export const runPy = () => {
-  PythonShell.run(
-    "./py.py",
-    {
-      pythonOptions: ["-u"], // get print results in real-time,
-    },
-    (error: any, output: any) => {
-      console.log({ fuuu: error, output });
-    }
-  );
 };
