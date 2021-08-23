a6ae89070645023f1a6cdbfef00333d8ba919b40
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 19:52:09 +0300

    dkdkkd

--- src/utils/getGps.js
+++ src/utils/getGps.js
@@ -2,12 +2,12 @@ const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
 const path = require("path");
 
-const getGPS = async (script) => {
+async function getGPS(script) {
   console.log("got her");
   const output = await exec(`python ${path.resolve(__dirname, "lat" + ".py")}`);
   console.log({ ssssss: script });
   return output.stdout.replace("\n", "");
-};
+}
 
 getGPS.then(console.log).catch(console.log);
 
---
+++ src/utils/motoron.py
@@ -0,0 +1,10 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+while True:
+    GPIO.output(11, 1)
+    quit()
+
