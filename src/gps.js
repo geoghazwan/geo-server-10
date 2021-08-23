0ca115cdf2b6cc87b76465084687d14b9c79ead2
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 14:38:33 +0300

    dkdk

--- src/gps.js
+++ src/gps.js
@@ -1,6 +1,12 @@
 const SerialPort = require("serialport");
 const port = new SerialPort("/dev/ttyUSB1");
 
+var GPS = require("gps");
+var gps = new GPS();
+gps.on("data", function (data) {
+  console.log(data, gps.state);
+});
+
 port.on("data", function (data) {
   console.log({ data });
 });
