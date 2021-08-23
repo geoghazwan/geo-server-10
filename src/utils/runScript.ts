289ee0f1a8aa3a2c2d9c9c410f93b936b4fab01a
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 17:01:16 +0300

    Gkgkg

--- src/utils/runScript.ts
+++ src/utils/runScript.ts
@@ -1,9 +1,9 @@
 import { promisify } from "util";
 const exec = promisify(require("child_process").exec);
 import { PythonShell } from "python-shell";
-
+import path from "path"
 export const runScript = async (script: string): Promise<void> => {
-  const output = await exec("python ./py2.py");
+  const output = await exec(`python ${path.resolve(__dirname, "py2.py")});
   console.log({ ooooooooo: output });
   return output;
 };
