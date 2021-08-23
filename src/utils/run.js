68d233ccc9cf722f8c1878c11539ae000a9f0482
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 17:08:32 +0300

    Gkgkg

--- src/utils/run.js
+++ src/utils/run.js
@@ -2,7 +2,7 @@ const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
 const path = require("path");
 
-export const runScript = async (script) => {
+const runScript = async (script) => {
   const output = await exec(`python ${path.resolve(__dirname, "py2.py")}`);
   console.log({ ooooooooo: output });
   return output;
