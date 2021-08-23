0795d9ea93a1698bfa2294c1222952f5952c7d31
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 15:21:11 +0300

    Gkgkg

--- src/py.py
+++ src/py.py
@@ -3,11 +3,14 @@ import time
 
 
 def main():
-    session = gps.gps("127.0.0.1", "2947")
-    session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-    raw_data = session.next()
-    if raw_data["class"] == "TPV":
-        print(raw_data)
+    while True: 
+        session = gps.gps("127.0.0.1", "2947")
+        session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+        raw_data = session.next()
+        if raw_data["class"] == "TPV":
+            print raw_data
+            quit()
+
 
 if __name__ == "__main__":
     main()
