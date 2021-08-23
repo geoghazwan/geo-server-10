301fc83d45c7f43e81188df12b8f25c59965b3f9
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-23 15:30:23 +0300

    Gkgkg

--- src/py.py
+++ src/py.py
@@ -1,11 +1,11 @@
 import gps
 import time
+session = gps.gps("127.0.0.1", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
 
 def main():
     while True: 
-        session = gps.gps("127.0.0.1", "2947")
-        session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
         raw_data = session.next()
         if raw_data["class"] == "TPV":
             if hasattr(raw_data, 'lat') and hasattr(raw_data, 'lon') and hasattr(raw_data, 'speed'):
--- src/py/tests.py
+++ src/py/tests.py
@@ -5,4 +5,15 @@ import time
 session = gps.gps('127.0.0.1', '2947')
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-
+    try:
+        time.sleep(0.5)
+        raw_data = session.next()
+        if raw_data['class'] == 'TPV':
+            if hasattr(raw_data, 'lat'):
+                result["lat"] = str(raw_data.lat)
+                print str(raw_data.lat)
+                quit()
+       
+    except StopIteration:
+        session = None
+        print 'No incoming data from the GPS module'
