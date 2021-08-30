70fee359bf75750dece3a0148b29f6cdfc292353
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2021-08-30 19:03:54 +0300

    TRAD-2468: [feature] withdrawal modal

--- src/routes/api/motor.js
+++ src/routes/api/motor.js
@@ -14,39 +14,39 @@ router.get("/start/:id", async (req, res) => {
   const { lat, lon, speed } = await getAllData();
   console.log({ lat, lon, speed });
   await startCar();
-  const location = new Location({
-    geo: { lat, long: lon },
-    speed,
-    date: new Date(),
-  });
-  console.log({ speed });
-  await location.save();
-  const destination = new Location({
-    geo: { lat, long: lon },
-    speed,
-    date: new Date(),
-  });
-  await destination.save();
-  const car = await Car.findById(req.params.id);
-  car.lastLocation = car.currentLocation;
-  car.currentLocation = location._id;
-  car.destination = destination._id;
-  await car.save();
-  res.status(200).send(car);
+  //   const location = new Location({
+  //     geo: { lat, long: lon },
+  //     speed,
+  //     date: new Date(),
+  //   });
+  //   console.log({ speed });
+  //   await location.save();
+  //   const destination = new Location({
+  //     geo: { lat, long: lon },
+  //     speed,
+  //     date: new Date(),
+  //   });
+  //   await destination.save();
+  //   const car = await Car.findById(req.params.id);
+  //   car.lastLocation = car.currentLocation;
+  //   car.currentLocation = location._id;
+  //   car.destination = destination._id;
+  //   await car.save();
+  res.status(200).send({});
 });
 
 router.get("/stop/:id", async (req, res) => {
   await stopCar();
   const { lat, lon, speed } = await getAllData();
-  const location = new Location({
-    geo: { lat, long: lon },
-    speed,
-    date: new Date(),
-  });
-  const car = await Car.findById(req.params.id);
-  car.currentLocation = location._id;
-  await car.save();
-  res.status(200).send(car);
+  //   const location = new Location({
+  //     geo: { lat, long: lon },
+  //     speed,
+  //     date: new Date(),
+  //   });
+  //   const car = await Car.findById(req.params.id);
+  //   car.currentLocation = location._id;
+  //   await car.save();
+  res.status(200).send({});
 });
 
 module.exports = router;
